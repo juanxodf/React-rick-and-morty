a6b7f2cecb3686702a15aa3e9e7ba872b3a0c595
juanxodf <martinpozuelojuan@gmail.com>
2026-01-22 19:28:18 +0100

    feat: Finalizo la HU02
    Después de un buen rato intentando como comprobar que funcionara está manera de crear un evento de espera mientras que se hace la carga de los personajes por así decirlo, la única manera que he tenido de comprobarlo ha sido quitando la i de la ruta de la api para que no encontrára la api y así darle más tiempo al evento.

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,31 @@ import CharacterList from "./components/CharacterList";
 
 export default function App() {
   const [characters, setCharacters] = useState<Character[]>([]);
-
+  const [loading, setLoading] = useState(false);
+  
   useEffect(() => {
+    setLoading(true);
+
     fetchCharacters({ page: 1 }).then((data) => {
       setCharacters(data.results);
+    }).finally(() => {
+      setLoading(false);
     });
   }, []);
 
   return (
     <div>
       <h1>Rick y Morty</h1>
-      <CharacterList characters={characters} />
+
+      {
+        // Muestra un mensaje si no hay personajes y no está cargando, pero si está cargando muestra la lista de personajes 
+      }
+
+      {loading && <p>Cargando personajes...</p>}
+
+      {!loading && (
+        <CharacterList characters={characters} />
+      )}
     </div>
   );
 }
