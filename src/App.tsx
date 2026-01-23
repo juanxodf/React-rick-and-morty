b7fa07b1eddd58af522b6fee4076e509b03ca9b1
juanxodf <martinpozuelojuan@gmail.com>
2026-01-23 01:27:46 +0100

    feat: Finalizo la HU05
    Lo he explicado en el archivo .md de Progreso del proyecto, pero vamos que esto de programar sin descansar, creo que no es bueno

--- src/App.tsx
+++ src/App.tsx
@@ -9,33 +9,37 @@ export default function App() {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState<string | null>(null);
   const [query, setQuery] = useState("");
+
+  const handleQueryChange = (newValue: string) => {
+    console.log("Nueva consulta:", newValue);
+    setQuery(newValue);
+  }
   
   useEffect(() => {
     setLoading(true);
     setError(null);
 
-    fetchCharacters({ page: 1 })
+    fetchCharacters({ page: 1 , name: query.trim()})
     .then((data) => { setCharacters(data.results) }) 
     .catch(() => { setError("Error al cargar los personajes") })
     .finally(() => { setLoading(false) });
-  }, []);
+  }, [query]);
 
   return (
     <div>
       <h1>Rick y Morty</h1>
 
-      <SearchBar value={query} onChange={setQuery}></SearchBar>
-
-      {
-        // Muestra un mensaje si no hay personajes y no está cargando, pero si está cargando muestra la lista de personajes 
-      }
+      <SearchBar value={query} onChange={handleQueryChange} />
 
       {loading && <p>Cargando personajes...</p>}
 
       {error && <p>{error}</p>}
 
       {!loading && !error && (
-        <CharacterList characters={characters} />
+        <>
+        <p> Resultados: {characters.length}</p>
+        <CharacterList characters={characters} />  
+      </>
       )}
     </div>
   );
